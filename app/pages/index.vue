2b2074f1301e0aa6061a645babdd6db8574f0b16
danijel374 <danijelprskalo7@gmail.com>
2024-12-17 01:28:41 +0100

    check

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,31 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  const { loggedIn, user, clear } = useUserSession();
+  const exampleProject = {
+    name: "",
+    difficulty: "newbie",
+    languages: ["html", "css"],
+  };
+</script>
 
 <template>
-  <h1>Projects bellow here!</h1>
-  <ul>
-    <li></li>
-  </ul>
+  <main class="container mx-auto">
+    <h1 class="text-2xl">Projects bellow here!</h1>
+    <ul>
+      <UButton
+        v-if="!loggedIn"
+        to="/api/auth/github"
+        icon="i-simple-icons-github"
+        label="Login with GitHub"
+        size="xs"
+        external
+      />
+    </ul>
+
+    <form action="">
+      <input type="text" />
+      <button>Add a project here</button>
+    </form>
+  </main>
 </template>
 
 <style></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   future: { compatibilityVersion: 4 },
   devtools: { enabled: true },
+  modules: ["@nuxt/ui", "nuxt-auth-utils", "@nuxthub/core"],
+  auth: {},
+  css: ["~/assets/css/main.css"],
 });
